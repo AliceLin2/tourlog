b974dd70fee2b92c50151b387d0f77fe2f5acfdd
shu Lin <bbqlinshu@gmail.com>
2022-06-10 09:31:01 -0400

    updated

--- client/src/components/City.js
+++ client/src/components/City.js
@@ -2,29 +2,26 @@ import React from 'react'
 import { Card, Icon, Image } from 'semantic-ui-react'
 
 function City({city}){
-  if(city.views.length===0)
-    return null
-  else
-    return(
-        <Card style={{ margin: '0.6rem' }}>
-          <Image src={city.views[0].images[0].url} wrapped ui={false} />
-          <Card.Content>
-            <Card.Header>{city.name}</Card.Header>
-            <Card.Meta>
-              <span>{city.country}, {city.area}</span>
-            </Card.Meta>
-            <Card.Description>
-                <p>Best month to visit {city.name}:</p> 
-                {city.seasons.map(s=><React.Fragment key={s.id}>{s.text} </React.Fragment>)}
-                <p>{city.name} is famous for:</p> 
-                {city.styles.map(s=><React.Fragment key={s.id}>{s.text} </React.Fragment>)}
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-              <Icon name='map' />
-              {city.views.length} Views
-          </Card.Content>
-        </Card>
+  return(
+      <Card style={{ margin: '0.6rem' }}>
+        {city.views.length===0?null:<Image src={city.views[0].images[0].url} wrapped ui={false}/>}
+        <Card.Content>
+          <Card.Header>{city.name}</Card.Header>
+          <Card.Meta>
+            <span>{city.country}, {city.area}</span>
+          </Card.Meta>
+          <Card.Description>
+              <p>Best month to visit {city.name}:</p> 
+              {city.seasons.map(s=><React.Fragment key={s.id}>{s.text} </React.Fragment>)}
+              <p>{city.name} is famous for:</p> 
+              {city.styles.map(s=><React.Fragment key={s.id}>{s.text} </React.Fragment>)}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+            <Icon name='map' />
+            {city.views.length} Views
+        </Card.Content>
+      </Card>
 )}
 
 export default City
